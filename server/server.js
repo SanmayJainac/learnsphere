86fe34be6c9c91bd4c9e1fcc44e45979b00f7f07
SanmayJainac <sanmayjainac@gmail.com>
2025-06-15 00:42:25 +0530

    Update server.js for Vercel deployment with serverless handler

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ import { clerkMiddleware } from '@clerk/express'
 import connectCloudinary from './configs/cloudinary.js'
 import courseRouter from './routes/courseRoute.js'
 import userRouter from './routes/userRoutes.js'
+import serverless from 'serverless-http';
 
 //initialize express
 const app=express()
@@ -27,10 +28,12 @@ app.use('/api/course',express.json(),courseRouter)
 app.use('/api/user',express.json(),userRouter)
 app.post('/stripe',express.raw({type:'application/json'}),stripeWebhooks)
 //port     
-const PORT=process.env.PORT||5000
+if (process.env.NODE_ENV !== 'production') {
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => {
+      console.log(`🚀 Server running locally on port ${PORT}`);
+    }
+);
+}
 
-//listen
-app.listen(PORT,()=>{
-    console.log(`Server running on port ${PORT}`)
-
-})
+export const handler = serverless(app);
