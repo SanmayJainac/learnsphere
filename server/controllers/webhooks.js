50d1153bdf7b63603355457441a213c4726dbc92
SanmayJainac <sanmayjainac@gmail.com>
2025-06-15 01:03:41 +0530

    Fix Stripe initialization for ESM

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -54,7 +54,9 @@ export const clerkWebhooks = async (req, res) => {
     }
 }
 
-const stripeInstance=new Stripe(process.env.STRIPE_SECRET_KEY)
+const stripeInstance = new Stripe(process.env.STRIPE_SECRET_KEY, {
+  apiVersion: '2023-10-16',
+});
 
 export const stripeWebhooks = async (request,response) => {
       const sig = request.headers['stripe-signature'];
